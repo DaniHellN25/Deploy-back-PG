47962a65dde6df631c28154e2b3a57f8b175c2b6
Pedro Daniel Chi Novelo <95224384+DaniHellN25@users.noreply.github.com>
2022-06-29 19:36:26 -0500

    Update userClientRoutes.js

--- build/routes/userClient/userClientRoutes.js
+++ build/routes/userClient/userClientRoutes.js
@@ -31,7 +31,7 @@ clientRouter.delete('/deleteuserclient', validateClient, deleteUserClient);
 clientRouter.put('/editprofile', validateClient, putUserClient);
 clientRouter.get('/auth/google/callback', passport.authenticate('google'), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     if (req.user) {
-       const user = req.user.role === 'client' ? yield userClients_1.default.findOne({ email: req.user.email }) : yield userPsychologist_1.default.findOne({ email: req.user.email });
+        const user = req.user.role === 'client' ? yield userClients_1.default.findOne({ email: req.user.email }) : req.user.role === 'psychologist' ? yield userPsychologist_1.default.findOne({ email: req.user.email }) : null;
         const userForToken = {
             id: user === null || user === void 0 ? void 0 : user._id,
             role: user === null || user === void 0 ? void 0 : user.role
